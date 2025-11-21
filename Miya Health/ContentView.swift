c3b6b705f5a00992593a6cd1083421cbc1f27028
Josh Kempton <hello@josh-kempton.com>
2025-11-21 14:52:36 +0400

    Initial Commit

---
+++ Miya Health/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Miya Health
+//
+//  Created by Josh Kempton on 21/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
